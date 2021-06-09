81535259cec1e3bfb5c43135b648d6c11293d163
jinook929 <jinook929@gmail.com>
2021-06-09 19:52:47 -0400

    open user profile route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authorized, only: [:index, :create]
+  skip_before_action :authorized, only: [:index, :show, :create]
 
   def index
     users = User.all
