489ac421ec48c2880e409b4f5d4d9bc4c14222f4
jinook929 <jinook929@gmail.com>
2021-06-09 19:35:28 -0400

    update database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_06_01_024440) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.integer "score"
     t.integer "user_id"
